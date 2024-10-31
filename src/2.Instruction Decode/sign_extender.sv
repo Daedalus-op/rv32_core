d71b8087311add7d8227fd7964283bc50c6fcec9
pranavbhat3 <65478365+pranavbhat3@users.noreply.github.com>
2024-10-31 22:35:02 +0530

    Update sign_extender.sv

--- src/2.Instruction Decode/sign_extender.sv
+++ src/2.Instruction Decode/sign_extender.sv
@@ -1,7 +1,7 @@
 
 module sign_extend(
         output logic [31:0]imm_out,
-        input logic [11:0] imm_in,
+        input logic [19:0] imm_in,
 	input logic [6:0] opcode
     );
 always_comb
@@ -12,18 +12,27 @@ always_comb
              end
         7'b00000011 :        // this is for load 
          begin  
-           imm_out[31:0]={{20{imm_in[11]}}, imm_in};
+           imm_out[31:0]={{20{imm_in[11]}}, imm_in[11:0]};
          end
          7'b0010011: begin //I Type 
-           imm_out[31:0]={{20{imm_in[11]}}, imm_in};
+           imm_out[31:0]={{20{imm_in[11]}}, imm_in[11:0]};
          end 
         7'b0100011:
         begin     // store
-         imm_out[31:0]={{20{imm_in[11]}}, imm_in};
+         imm_out[31:0]={{20{imm_in[11]}}, imm_in[11:0]};
         end
         7'b1100011:
         begin              
-         imm_out[31:0]={{20{imm_in[11]}},imm_in};
+         imm_out[31:0]={{20{imm_in[11]}},imm_in[11:0]};
         end      
+        7'b0110111||7'b0010111:
+        begin
+        
+         imm_out[31:0]={{12{imm_in[19]}},imm_in};
+        end
+        7'b1101111||7'b1100111:
+        begin 
+                         imm_out[31:0]={{12{imm_in[19]}},imm_in};    
+        end 
           endcase
 endmodule
