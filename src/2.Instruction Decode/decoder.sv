d56f32b280c3215d5186ffbb0cce8af225264833
pranavbhat3 <65478365+pranavbhat3@users.noreply.github.com>
2024-10-31 22:34:41 +0530

    Update decoder.sv

--- src/2.Instruction Decode/decoder.sv
+++ src/2.Instruction Decode/decoder.sv
@@ -5,61 +5,50 @@ module decoder(
     output logic [4:0]  rs1,         // Source register 1
     output logic [4:0]  rs2,         // Source register 2
     output logic [6:0]  funct7,      // Funct7 field (for R-type)
-    output logic [11:0] immediate,   // Immediate Value
+    output logic [19:0] immediate,   // Immediate Value
     input  logic [31:0] instruction  // 32-bit instruction
 );
 assign opcode=instruction[6:0];
+assign immediate=20'b0;
 always_comb
     case(opcode)
-        7'b0110011:		// R-type
+        7'b0110011:                //this is for R type
+            begin
+                 rd=instruction[11:7];
+                 funct3=instruction[14:12];
+                 rs1=instruction[19:15];
+                 rs2=instruction[24:20];
+                 funct7=instruction[31:25];
+             end
+        7'b00000011 || 7'b0010011:        // this is for load and I Type , doubt for shift
+         begin  
+                 rd=instruction[11:7];
+                 funct3=instruction[14:12];
+                 rs1=instruction[19:15];         
+         end
+        7'b0100011:
+        begin     // store
+         funct3=instruction[14:12];
+         rs1=instruction[19:15];
+         rs2=instruction[24:20];
+	 immediate={instruction[31:25],instruction[11:7]};
+        end
+        7'b1100011:
+        begin              
+                 funct3=instruction[14:12];
+                 rs1=instruction[19:15];
+                 rs2=instruction[24:20];
+		 immediate={instruction[7], instruction[30:25],instruction[11:8]};
+        end      
+        7'b0110111||7'b0010111:
         begin
-		rd=instruction[11:7];
-		funct3=instruction[14:12];
-		rs1=instruction[19:15];
-		rs2=instruction[24:20];
-		funct7=instruction[31:25];
+               rd=instruction[11:7];
+               immediate=instruction[31:12];
         end
-        7'b0000011: 		// I-type
-        begin
-		rd=instruction[11:7];
-		funct3=instruction[14:12];
-		rs1=instruction[19:15];
-		immediate=instruction[31:20];
-        end
-        7'b0010011:		// Load
-        begin  
-		rd=instruction[11:7];
-		funct3=instruction[14:12];
-		rs1=instruction[19:15];
-		immediate=instruction[31:20];
-        end
-        7'b0100011:		// S-Type
-        begin
-		funct3=instruction[14:12];
-		rs1=instruction[19:15];
-		rs2=instruction[24:20];
-		immediate={instruction[31:25],instruction[11:7]};
-	end
-	7'b1100011:		// B-Type
-	begin
-		funct3=instruction[14:12];
-		rs1=instruction[19:15];
-		rs2=instruction[24:20];
-		immediate={instruction[31], instruction[7], instruction[30:25],instruction[11:8]};
-        end
-	7'b:			// U-Type !
-	begin
-		funct3=instruction[14:12];
-		rs1=instruction[19:15];
-		rs2=instruction[24:20];
-		immediate={instruction[31], instruction[7], instruction[30:25],instruction[11:8]};
-        end
-	7'b:			// J-Type !
-	begin
-		funct3=instruction[14:12];
-		rs1=instruction[19:15];
-		rs2=instruction[24:20];
-		immediate={instruction[31], instruction[7], instruction[30:25],instruction[11:8]};
-        end
+        7'b1101111||7'b1100111:
+        begin 
+                       rd=instruction[11:7];
+	 immediate={instruction[20], instruction[10:1],instruction[1],instruction[19:12]};
+        end 
           endcase
 endmodule
